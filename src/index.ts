431e380aa1a9ab3d708200111776d8b77c476c75
Mayur Soni <mayursoni@Ayushh-M-Soni.local>
2025-11-19 00:08:24 +0530

    boilerplate ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export default class RateLimiter {
+    private request: Map<string, number> = new Map();
+    private readonly maxRequests: number;
+    private readonly windowMs: number;
+
+    constructor(maxRequest: number, windowms: number){
+        this.maxRequests = maxRequest;
+        this.windowMs = windowms;
+    }
+    
+    isAllowed(userId: string): boolean {
+        return true;
+    }
+}
